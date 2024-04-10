74542db6dd2d0376a8c594ec0ddfc319da94cd9d
Pockylee <brian7149@gmail.com>
2024-04-09 17:46:47 -0700

    Initial Commit

---
+++ stock_app/stock_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  stock_appApp.swift
+//  stock_app
+//
+//  Created by Brian Li on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct stock_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
