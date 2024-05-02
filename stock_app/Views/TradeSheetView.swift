8f6b8892a38c40dda53e6aec51148568a334d77c
Pockylee <brian7149@gmail.com>
2024-05-02 13:31:03 +0800

    trade sheet finished

--- stock_app/Views/TradeSheetView.swift
+++ stock_app/Views/TradeSheetView.swift
@@ -20,7 +20,9 @@ struct TradeSheetView: View {
     
     var body: some View {
         VStack {
+            
             Text("Trade \(viewModel.companyName) shares")
+                .padding(.top)
             Spacer()
             
             HStack {
@@ -32,6 +34,7 @@ struct TradeSheetView: View {
                     .font(.largeTitle)
             }
             
+            
             HStack {
                 Spacer()
                 Text("x $\(viewModel.currentPrice, specifier: "%.2f")/share = \(totalCost, specifier: "$%.2f")")
@@ -41,10 +44,27 @@ struct TradeSheetView: View {
             Spacer()
             
             VStack {
+                if showingToast {
+                    Text(toastMessage)
+                        .padding()
+                        .background(Color.gray)
+                        .foregroundColor(.white)
+                        .transition(.slide)
+                        .cornerRadius(30) // Apply a corner radius to the
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                withAnimation {
+                                    showingToast = false
+                                }
+                            }
+                        }
+                }
                 Text("\(portfolioViewModel.cashBalance, specifier: "$%.2f") available to buy \(viewModel.stockSymbol)")
                     .font(.caption)
                     .foregroundColor(Color.gray)
             }
+            
+            
             
             HStack {
                 Button("Buy") {
@@ -68,22 +88,6 @@ struct TradeSheetView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 30))
             }
             .padding(.vertical)
-            
-            
-            if showingToast {
-                Text(toastMessage)
-                    .padding()
-                    .background(Color.red)
-                    .foregroundColor(.white)
-                    .transition(.slide)
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                            withAnimation {
-                                showingToast = false
-                            }
-                        }
-                    }
-            }
         }
         .overlay(
             successView
@@ -134,9 +138,12 @@ struct TradeSheetView: View {
     private func attemptToBuyShares() {
         if let shares = Int(numberOfShares), shares > 0 {
             if Double(shares) * viewModel.currentPrice <= portfolioViewModel.cashBalance {
-                // Execute the buy transaction
-                successMessage = "You have successfully sold \(numberOfShares) \(numberOfSharesAsInt == 1 ? "share" : "shares") of \(viewModel.stockSymbol)"
-                showingSuccessView = true
+                sendBuyRequest(shares: shares, totalCost: totalCost)
+                let change = -totalCost
+                updateWallet(change: change) {
+                    portfolioViewModel.cashBalance += change
+                }
+                
             } else {
                 showToast(message: "Not enough money to buy")
             }
@@ -148,9 +155,12 @@ struct TradeSheetView: View {
     private func attemptToSellShares() {
         if let shares = Int(numberOfShares), shares > 0 {
             if shares <= viewModel.sharesOwned {
-                // Execute the sell transaction
-                successMessage = "You have successfully bought \(numberOfShares) \(numberOfSharesAsInt == 1 ? "share" : "shares") of \(viewModel.stockSymbol)"
-                showingSuccessView = true
+                
+                sendSellRequest(shares: shares, totalCost: totalCost)
+                let change = totalCost
+                updateWallet(change: change) {
+                    portfolioViewModel.cashBalance += change
+                }
             } else {
                 showToast(message: "Not enough shares to sell")
             }
@@ -168,7 +178,139 @@ struct TradeSheetView: View {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
     
+    private func sendSellRequest(shares: Int, totalCost: Double) {
+        let url = URL(string: "\(APIConfig.baseURL)/portfolio/sell")! // Change to your actual server URL
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let requestBody: [String: Any] = [
+            "symbol": viewModel.stockSymbol,
+            "companyName": viewModel.companyName,
+            "quantity": shares,
+            "totalCost": totalCost
+        ]
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: requestBody)
+        } catch {
+            print("Error: cannot create JSON from requestBody")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                DispatchQueue.main.async {
+                    self.showToast(message: "Failed to sell shares due to wallet database connect problem")
+                }
+                return
+            }
+            
+            if let jsonData = data {
+                do {
+                    let decodedResponse = try JSONDecoder().decode(PortfolioEntry.self, from: jsonData)
+                    DispatchQueue.main.async {
+                        self.viewModel.sharesOwned -= shares // Update local viewModel with new shares count
+                        self.successMessage = "You have successfully sold \(shares) \(shares == 1 ? "share" : "shares") of \(self.viewModel.stockSymbol)"
+                        self.showingSuccessView = true
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        self.showToast(message: "Error decoding response")
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.showToast(message: "No data received from server")
+                }
+            }
+        }.resume()
+    }
     
+    private func sendBuyRequest(shares: Int, totalCost: Double) {
+        let url = URL(string: "\(APIConfig.baseURL)/portfolio")! // Change to your actual server URL
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let requestBody: [String: Any] = [
+            "symbol": viewModel.stockSymbol,
+            "companyName": viewModel.companyName,
+            "quantity": shares,
+            "totalCost": totalCost
+        ]
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: requestBody)
+        } catch {
+            print("Error: cannot create JSON from requestBody")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                DispatchQueue.main.async {
+                    self.showToast(message: "Failed to buy shares due to wallet database connect problem")
+                }
+                return
+            }
+            
+            if let jsonData = data {
+                do {
+                    let decodedResponse = try JSONDecoder().decode(PortfolioEntry.self, from: jsonData)
+                    DispatchQueue.main.async {
+                        self.viewModel.sharesOwned += shares // Update local viewModel with new shares count
+                        self.successMessage = "You have successfully bought \(shares) \(shares == 1 ? "share" : "shares") of \(self.viewModel.stockSymbol)"
+                        self.showingSuccessView = true
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        self.showToast(message: "Error decoding response")
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.showToast(message: "No data received from server")
+                }
+            }
+        }.resume()
+    }
+    
+    private func updateWallet(change: Double, completion: @escaping () -> Void) {
+        let url = URL(string: "\(APIConfig.baseURL)/wallet")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "PATCH"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let requestBody: [String: Any] = ["change": change]
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: requestBody)
+        } catch {
+            print("Error: cannot create JSON from requestBody")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                DispatchQueue.main.async {
+                    showToast(message: "Failed to update wallet")
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion()
+            }
+        }.resume()
+    }
+}
+
+
+struct PortfolioEntry: Decodable {
+    var symbol: String
+    var quantity: Int
+    var totalCost: Double
+    var averageCost: Double? // Include other fields as per your API response
 }
 
 #Preview {
